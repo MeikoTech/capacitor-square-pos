94e1e4a6d3361665f3b4a82ea4f745853fcf9b1a
Rick Meima <rickmeima@spyce.ca>
2025-01-06 13:58:41 -0800

    podspec

--- src/web.ts
+++ src/web.ts
@@ -1,23 +1,31 @@
-import { WebPlugin } from '@capacitor/core';
+import { PluginListenerHandle, WebPlugin } from '@capacitor/core';
 
 import type { AutoReturn, CapacitorSquarePlugin } from './definitions';
 
 export class CapacitorSquareWeb extends WebPlugin implements CapacitorSquarePlugin {
-  async initApp(_options: { applicationId: string; }): Promise<{ message: string }> {
-    throw new Error("Method not implemented.");
+  addListener(): Promise<PluginListenerHandle> & PluginListenerHandle {
+    const handle: PluginListenerHandle = {
+      remove: async () => {
+        // Implementation for removing the listener
+      },
+    };
+    return Object.assign(Promise.resolve(handle), handle);
+  }
+  async initApp(_options: { applicationId: string }): Promise<{ message: string }> {
+    throw new Error('Method not implemented.');
   }
 
   async startTransaction(_options: {
-    totalAmount: number,
-    currencyCode: string,
-    allowedPaymentMethods?: string[] | null,
-    autoReturnTimeout?: number | AutoReturn.NoTimeout | null,
-    callbackUrl?: string | null
+    totalAmount: number;
+    currencyCode: string;
+    allowedPaymentMethods?: string[] | null;
+    autoReturnTimeout?: number | AutoReturn.NoTimeout | null;
+    callbackUrl?: string | null;
   }): Promise<void> {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.');
   }
 
-  async handleIosResponse(_options: { url: string; }): Promise<void> {
-    throw new Error("Method not implemented.");
+  async handleIosResponse(_options: { url: string }): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
